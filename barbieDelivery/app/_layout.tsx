b5aee0d3dd572b9fefcd614d47346d8655a26d2d
nickicn <nicolelopes0008@gmail.com>
2025-09-01 10:43:59 -0300

    CustomHeader finalizado :D

--- barbieDelivery/app/_layout.tsx
+++ barbieDelivery/app/_layout.tsx
@@ -1,14 +1,20 @@
 import CustomHeader from "@/components/CustomHeader";
 import { Stack } from "expo-router";
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
-  return(
-    <Stack>
-      <Stack.Screen 
-      name="index"  
-      options={{ header: () => <CustomHeader/> }}
-      />
-    </Stack> 
-    );
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }} >
+      <BottomSheetModalProvider>
+        <Stack>
+          <Stack.Screen
+            name="index"
+            options={{ header: () => <CustomHeader /> }}
+          />
+        </Stack>
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
+  );
 }
 
--- barbieDelivery/components/CustomHeader.tsx
+++ barbieDelivery/components/CustomHeader.tsx
@@ -4,30 +4,44 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 import Colors from '@/constants/Colors';
 import { Link } from 'expo-router';
 
+const SearchBar = () =>
+  <View style={styles.searchContainer}>
+    <View style={styles.searchField}>
+      <Ionicons style={styles.searchIcon} name='search' size={25} color={Colors.medium} />
+      <TextInput style={styles.input} placeholder='Restaurante, lanchonetes, pratos' />
+    </View>
+    <Link href={'/'} asChild>
+      <TouchableOpacity style={styles.optionButton}>
+        <Ionicons name='options-outline' size={25} color={Colors.primary} />
+      </TouchableOpacity>
+    </Link>
+  </View>
+
 export default function CustomHeader() {
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
-      <TouchableOpacity>
-        <Image
-        style={styles.bike}
-        source={require('@/assets/images/bike.png')}
-        />
-      </TouchableOpacity>
-      <View style={styles.titlecontainer}>
-        <Text style={styles.title}>Entregando</Text>
-        <TouchableOpacity style={styles.location}>
-          <Text style={styles.subtitle}>
-            Barra Bonita, SP
-          </Text>
-          <Ionicons name='chevron-down' size={25} color={Colors.primary}/>
+        <TouchableOpacity>
+          <Image
+            style={styles.bike}
+            source={require('@/assets/images/bike.png')}
+          />
+        </TouchableOpacity>
+        <View style={styles.titlecontainer}>
+          <Text style={styles.title}>Entregando</Text>
+          <TouchableOpacity style={styles.location}>
+            <Text style={styles.subtitle}>
+              Barra Bonita, SP
+            </Text>
+            <Ionicons name='chevron-down' size={25} color={Colors.primary} />
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity style={styles.profileButton}>
+          <Ionicons name='person-outline' size={25} color={Colors.primary}
+          />
         </TouchableOpacity>
       </View>
-      <TouchableOpacity  style={styles.profileButton}>
-        <Ionicons name='person-outline' size={25} color={Colors.primary}
-      />
-      </TouchableOpacity>
-      </View>
+      <SearchBar />
     </SafeAreaView>
   )
 }
@@ -68,9 +82,35 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginRight: 5,
   },
-  profileButton: {  
+  profileButton: {
     backgroundColor: Colors.lightGrey,
     padding: 10,
-    borderRadius: 50
-  }
+    borderRadius: 50,
+  },
+  searchContainer: {
+    height: 60,
+    flexDirection: 'row',
+    gap: 10,
+    backgroundColor: '#fff',
+    paddingHorizontal: 20,
+    alignItems: 'center',
+  },
+  searchField: {
+    flex: 1,
+    backgroundColor: Colors.lightGrey,
+    borderRadius: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  input: {
+    padding: 10,
+    color: Colors.mediumDark,
+  },
+  searchIcon: {
+    padding: 10,
+  },
+  optionButton: {
+    padding: 10,
+    borderRadius: 50,
+  },
 })
